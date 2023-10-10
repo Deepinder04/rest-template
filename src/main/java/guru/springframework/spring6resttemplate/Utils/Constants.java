6d18efc149e2f32aa6eb33a8d980a1386688aded
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-10-10 20:37:47 +0530

    saving external api response as string, map and json node

---
+++ src/main/java/guru/springframework/spring6resttemplate/Utils/Constants.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring6resttemplate.Utils;
+
+public class Constants {
+    public static final String BASE_URL = "http://localhost:8080";
+    public static final String GET_BEER_PATH = "/api/v1/beer";
+}
