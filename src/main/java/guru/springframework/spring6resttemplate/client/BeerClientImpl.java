fd0496dede5d9961cbdad1d314c436a020de6570
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-10-11 07:51:40 +0530

    saving external api response using a page implementation of BeerDTOs

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -1,15 +1,13 @@
 package guru.springframework.spring6resttemplate.client;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import guru.springframework.spring6resttemplate.model.BeerDTO;
+import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 import static guru.springframework.spring6resttemplate.Utils.Constants.BASE_URL;
 import static guru.springframework.spring6resttemplate.Utils.Constants.GET_BEER_PATH;
@@ -25,21 +23,10 @@ public class BeerClientImpl implements BeerClient {
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        ResponseEntity<String> stringResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, String.class);
+        ResponseEntity<BeerDTOPageImpl> pageResponse =
+                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, BeerDTOPageImpl.class);
 
-        ResponseEntity<Map> mapResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
-
-        ResponseEntity<JsonNode> jsonResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
-
-        jsonResponse.getBody().findPath("content")
-                .elements().forEachRemaining( node -> {
-                    System.out.println(node.get("beerName").asText());
-                });
-
-        System.out.println(mapResponse.getBody());
+        System.out.println(pageResponse.getBody());
 
         return null;
     }
