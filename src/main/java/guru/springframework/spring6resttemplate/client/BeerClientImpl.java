8b3cc4a02b14139134c7612cc3a527ee86165c5f
deepindersinghsindhu <deepinder.sidhu107@gmail.com>
2023-10-11 20:42:58 +0530

    added query parameters to the external service api call

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6resttemplate.client;
 
 import guru.springframework.spring6resttemplate.model.BeerDTO;
 import guru.springframework.spring6resttemplate.model.BeerDTOPageImpl;
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
@@ -10,7 +11,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import static guru.springframework.spring6resttemplate.Utils.Constants.BASE_URL;
 import static guru.springframework.spring6resttemplate.Utils.Constants.GET_BEER_PATH;
 
 
@@ -22,13 +22,40 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public Page<BeerDTO> listBeers() {
-        RestTemplate restTemplate = restTemplateBuilder.build();
+        return this.listBeers(null,null,null,null,null);
+    }
 
-        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
+    @Override
+    public Page<BeerDTO> listBeers(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize) {
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        String url = buildGetBeerUrl(beerName,beerStyle,showInventory,pageNumber,pageSize);
+
+        System.out.println(url);
 
         ResponseEntity<BeerDTOPageImpl> response =
-                restTemplate.getForEntity(uriComponentsBuilder.toUriString(), BeerDTOPageImpl.class);
+                restTemplate.getForEntity(url, BeerDTOPageImpl.class);
 
         return response.getBody();
+    }
+
+    public static String buildGetBeerUrl(String beerName, BeerStyle beerStyle, Boolean showInventory, Integer pageNumber, Integer pageSize){
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath(GET_BEER_PATH);
+
+        if(beerName!=null)
+            uriComponentsBuilder.queryParam("beerName",beerName);
+
+        if(beerStyle!=null)
+            uriComponentsBuilder.queryParam("beerStyle",beerStyle);
+
+        if(showInventory!=null)
+            uriComponentsBuilder.queryParam("showInventory",showInventory);
+
+        if(pageNumber!=null)
+            uriComponentsBuilder.queryParam("pageNumber",pageNumber);
+
+        if(pageSize!=null)
+            uriComponentsBuilder.queryParam("pageSize",pageSize);
+
+        return uriComponentsBuilder.toUriString();
     }
 }
--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -1,10 +1,9 @@
 package guru.springframework.spring6resttemplate.client;
 
+import guru.springframework.spring6resttemplate.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BeerClientImplTest {
@@ -14,7 +13,12 @@ class BeerClientImplTest {
 
     @Test
     void testListBeers(){
-        beerClient.listBeers();
+        beerClient.listBeers(null, BeerStyle.LAGER, null, null, null);
+    }
+
+    @Test
+    void testListBeersNoName(){
+        beerClient.listBeers(null, null, null, null, null);
     }
 
 }
